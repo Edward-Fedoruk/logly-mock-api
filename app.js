6a4bcf46cef3f1bf6aa52ccbd6034165264b9f96
Eduard Fedoruk <ed.fedorukk@gmail.com>
2020-03-19 19:35:09 +0100

    init

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const jsonParser = bodyParser.json();
+
+app.get("/test-status", (req, res) => {
+	res.sendStatus(200);
+});
+
+app.get("/test-data", (req, res) => {
+	res.send('Some data');
+});
+
+app.listen(80, () => {
+	console.log("Server is started...");
+});
